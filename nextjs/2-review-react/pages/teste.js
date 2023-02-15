0bc66e790f087c54d81e0ee24175f5921781a551
Caio Vinicius <caio.vinicius@ifood.com.br>
2023-02-15 20:57:59 -0300

    Add exemplo do curso de nextjs

---
+++ nextjs/2-review-react/pages/teste.js
@@ -0,0 +1,11 @@
+export default function teste(){
+
+    const teste = [
+        <li>caio</li>,
+        <li>joão</li>
+    ]
+
+    return (
+        teste
+    )
+}
---
+++ nextjs/README.md
@@ -0,0 +1,10 @@
+### Criando um projeto NEXT:
+
+```shell
+npx create-next-app <<nome da aplicação>>
+```
+
+## Executar o projeto
+```shell
+npm run dev
+```
